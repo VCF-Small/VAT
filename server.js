4411c0d786c0ed469bfe8d12b7f9cb62d974a3d6
ankit <ac2000choudhary@gmail.com>
2021-02-22 14:50:39 +0530

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'Views')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(session({ 
     secret: '123456cat',
